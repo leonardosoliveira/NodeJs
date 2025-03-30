d26f099edaa1667ff90565340773e41c2ede5648
Leonardo Oliveira <leonardosoliveira96@hotmail.com>
2025-03-30 16:12:00 -0300

    Adicionando tratativa de erros

--- api/operacoes.js
+++ api/operacoes.js
@@ -1,31 +1,47 @@
 const {somar, div, multi, sub} = require('../services/operacoes');
 
 function apiSomar(req, res){
-    const {num1, num2} = req.body
-    const result = somar(num1, num2)
-    
-    res.status(200).send({ result })
-};
+    try{
+        const {num1, num2} = req.body
+        const result = somar(num1, num2)
+        
+        res.status(200).send({ result })
+    }catch(error){
+        res.status(500).send({ msg: error.message})
+    }
+}
 
 function apiSubtrair(req, res){
-    const {num1, num2} = req.body
-    const result = sub(num1, num2)
+    try{
+        const {num1, num2} = req.body
+        const result = sub(num1, num2)
 
-    res.status(200).send({ result })
+        res.status(200).send({ result })
+    }catch(error){
+        res.status(500).send({msg: error.message})
+    }
 };
 
 function apiDivisao(req, res){
-    const {num1, num2} = req.body
-    const result = div(num1, num2)
+    try{
+        const {num1, num2} = req.body
+        const result = div(num1, num2)
 
-    res.status(200).send({ result })
+        res.status(200).send({ result })
+    }catch(error){
+        res.status(500).send({msg: error.message})
+    }
 };
 
 function apiMulti(req, res){
-    const {num1, num2} = req.body
-    const result = multi(num1, num2)
+    try{ 
+        const {num1, num2} = req.body
+        const result = multi(num1, num2)
 
-    res.status(200).send({ result })
+        res.status(200).send({ result })
+    }catch(error){
+        res.status(500).send({msg: error.message})
+    }
 };
 
 module.exports = { apiSomar, apiSubtrair, apiMulti, apiDivisao}
